1aa671702a541da6e8525f11644188f9831f6b1d
gabrielms <gabrielmatosdesouza@gmail.com>
2013-10-20 11:16:59 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infravision::Application.config.secret_token = '046b87f62cd78cf920495290cd2f3b881ed7c96ab966fc358833cdd8a35f2760b91c394319726b09a694880ee4aff2d267d962ee2c7c667947174846a442b7ee'
